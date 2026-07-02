0ba8513e15ab0a14fbd2fca02ac7d77b87e91fc5
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-07-02 18:51:37 +0530

    Minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Login from '../modules/auth/components/Login';
 import { RegisterForm } from '../modules/auth/components/Register';
 
 export default function Home() {
-  console.info('Hello world');
   return (
     <div className='flex gap-0.5'>
       <RegisterForm />
